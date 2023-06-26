cb1d7cb227f2c056ca0cd362fe86b69cf847ba34
Rodrigo Nardi <rnardi@netdef.org>
2023-06-26 07:27:20 -0300

    bgp: Using variable after free

--- bgpd/bgp_bfd.c
+++ bgpd/bgp_bfd.c
@@ -320,8 +320,10 @@ static void bgp_peer_remove_bfd(struct peer *p)
 		return;
 	}
 
-	if (p->bfd_config)
-			bfd_sess_free(&p->bfd_config->session);
+	if (p->bfd_config) {
+				bfd_sess_free(&p->bfd_config->session);
+				bgp_peer_bfd_reset(p);
+	}
 
 	XFREE(MTYPE_BFD_CONFIG, p->bfd_config);
 }

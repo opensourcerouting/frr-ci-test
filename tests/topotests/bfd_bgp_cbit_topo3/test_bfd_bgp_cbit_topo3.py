9b785c72f855512574ebaa4a7af86b5761463c66
Rodrigo Nardi <rnardi@netdef.org>
2023-08-08 08:43:00 -0300

    doc: adding debug

--- tests/topotests/bfd_bgp_cbit_topo3/test_bfd_bgp_cbit_topo3.py
+++ tests/topotests/bfd_bgp_cbit_topo3/test_bfd_bgp_cbit_topo3.py
@@ -84,6 +84,8 @@ def test_protocols_convergence():
     # Check IPv6 routing tables.
     logger.info("Checking IPv6 routes for convergence")
     for router in tgen.routers().values():
+        cmd = 'vtysh -c "debug bfd zebra" -c "debug bfd peers"'
+        router.cmd(cmd)
         if router.name == "r2":
             continue
         json_file = "{}/{}/ipv6_routes.json".format(CWD, router.name)

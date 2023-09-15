b5796cf410286c11a992a5e224300d803775f2c5
Rodrigo Nardi <rnardi@netdef.org>
2023-09-15 07:47:11 -0300

    doc: missing semicolon

--- bgpd/bgpd.c
+++ bgpd/bgpd.c
@@ -2055,6 +2055,7 @@ int peer_remote_as(struct bgp *bgp, union sockunion *su, const char *conf_if,
 		peer_create(su, conf_if, bgp, local_as, *as, as_type, NULL,
 			    true, as_str);
 	}
+
 	return 0;
 }
 
